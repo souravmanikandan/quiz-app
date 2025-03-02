93f586971d5fc81aba1a6c0c7201e459d3b3f67c
smcoderr <souravoz2018@gmail.com>
2025-03-02 16:37:28 +0530

    '

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/quizz-app/",
   plugins: [
     react(),
     tailwindcss()
   ],
+  base: "/quizz-app/"
 })
